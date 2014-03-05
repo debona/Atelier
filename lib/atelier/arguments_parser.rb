8411229f4803bb87d1bdb8a55ce53b90deb6fb01
Thomas DE BONA <thomas.debona@gmail.com>
2014-03-05 22:10:35 +0100

    Support variable arguments

--- lib/atelier/arguments_parser.rb
+++ lib/atelier/arguments_parser.rb
@@ -14,6 +14,14 @@ module Atelier
 
     def param(name)
       @arguments_range[name] = 0
+
+      @arguments_range.each do |name, arity|
+        @arguments_range[name] = Range.new(arity.begin, arity.end - 1) if arity.is_a? Range
+      end
+    end
+
+    def params(name)
+      @arguments_range[name] = 0..-1
     end
 
     def parse(*parameters)
--- spec/arguments_parser_spec.rb
+++ spec/arguments_parser_spec.rb
@@ -30,6 +30,19 @@ describe Atelier::ArgumentsParser do
       its(:first_param)  { should == :one }
       its(:second_param) { should == :two }
     end
+
+    context 'for variable arguments' do
+      before do
+        arguments_parser.param(:first_param)
+        arguments_parser.params(:var_params)
+        arguments_parser.param(:last_param)
+      end
+
+      its(:first_param) { should == :one }
+      its(:var_params)  { should == [:two, :three, :four] }
+      its(:last_param)  { should == :five }
+
+    end
   end
 
 end
