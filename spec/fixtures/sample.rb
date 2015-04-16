bab120fa58f5095916881ec421cc9fff22ed7da8
Thomas DE BONA <thomas.debona@gmail.com>
2015-04-16 18:06:08 +0200

    Adapt fixtures

--- spec/fixtures/sample.rb
+++ spec/fixtures/sample.rb
@@ -2,31 +2,42 @@
 
 require 'atelier'
 
-command :sample do
+command :sample do |sample|
 
-  load_command File.join(File.dirname(__FILE__), 'sub_sample.rb')
+  # sample.load_command File.join(File.dirname(__FILE__), 'sub_sample.rb')
 
-  title 'It is a sample'
-  description <<-EOS
+  sample.title 'It is a sample'
+  sample.description <<-EOS
     This sample is used by the test suite.
     This sample is used by the test suite.
     This sample is used by the test suite.
   EOS
 
-  action { run(:help) }
+  sample.action { sample.run(:help) }
 
-  command :sample_command do
-    title 'sample command'
-    description 'It will print all the arguments one by line.'
-    action do |*args|
+  sample.command :sample_command do |sample_command|
+    sample_command.title 'sample command'
+    sample_command.description 'It will print all the arguments one by line.'
+    sample_command.action do |*args|
       puts 'sample_command called!'
       args.each { |arg| puts "  - #{arg}" }
       args
     end
+
+    sample_command.command :sub_sample_command do |sub_sample_command|
+      sub_sample_command.title 'sample command'
+      sub_sample_command.description 'It will print all the arguments one by line.'
+      sub_sample_command.action do |*args|
+        puts 'sub_sample_command called!'
+        args.each { |arg| puts "  - #{arg}" }
+        args
+      end
+    end
   end
 
-  command :another_command do
-    action do |*args|
+
+  sample.command :another_command do |another_command|
+    another_command.action do |*args|
       puts 'another_command called!'
       args.each { |arg| puts "  * #{arg}" }
       args
--- spec/fixtures/sub_sample.rb
+++ spec/fixtures/sub_sample.rb
@@ -2,11 +2,11 @@
 
 require 'atelier'
 
-command :sub_sample do
-  title 'It is a sub command sample (nested in the sample command)'
+command :sub_sample do |sub_sample|
+  sub_sample.title 'It is a sub command sample (nested in the sample command)'
 
-  action do |*args|
-    puts 'sub_sample_action called!'
+  sub_sample.action do |*args|
+    puts 'sub_sample action called!'
     args.each { |arg| puts "  - #{arg}" }
     args
   end
