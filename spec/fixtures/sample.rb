dda0a8dc9bad271cb489304c5538c23e47026dc4
Thomas DE BONA <thomas.debona@gmail.com>
2015-05-06 13:47:53 +0200

    Add argument parsing and option parsing to the sample

--- spec/fixtures/sample.rb
+++ spec/fixtures/sample.rb
@@ -4,8 +4,6 @@ require 'atelier'
 
 command :sample do |sample|
 
-  sample.load_command File.join(File.dirname(__FILE__), 'sub_sample.rb')
-
   sample.title = 'It is a sample'
   sample.description = <<-EOS
     This sample is used by the test suite.
@@ -13,7 +11,27 @@ command :sample do |sample|
     This sample is used by the test suite.
   EOS
 
-  sample.action { sample.run(:help) }
+  sample.option(:alert, '-a', '--alert ALERT', 'Alert')
+
+  sample.param(:first)
+  sample.params(:middles)
+  sample.param(:last)
+
+  sample.action do |arguments|
+    puts 'arguments:'
+    puts arguments
+
+    puts 'first:'
+    puts arguments[:first]
+    puts 'middles:'
+    puts arguments[:middles]
+    puts 'last:'
+    puts arguments[:last]
+
+    puts 'options:'
+    puts sample.options
+  end
+
 
   sample.command :sample_command do |sample_command|
     sample_command.title = 'sample command'
@@ -35,7 +53,6 @@ command :sample do |sample|
     end
   end
 
-
   sample.command :another_command do |another_command|
     another_command.action do |*args|
       puts 'another_command called!'
@@ -43,5 +60,7 @@ command :sample do |sample|
       args
     end
   end
+
+  sample.load_command File.join(File.dirname(__FILE__), 'sub_sample.rb')
 
 end
