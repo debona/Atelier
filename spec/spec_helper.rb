c67328384a74bad51654a6a9b9fb94d2808bca26
Thomas DE BONA <thomas.debona@gmail.com>
2020-03-26 22:44:41 +0100

    Require byebug in specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,12 +5,12 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
+require 'byebug'
 require 'rspec/its'
 require 'coveralls'
 Coveralls.wear!
 
 RSpec.configure do |config|
-  config.fail_if_no_examples = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
   config.raise_errors_for_deprecations!
